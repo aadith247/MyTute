ee872add07d89509aac6ddddadb1761ece2ee954
priyanshuraj27 <priyanshuraj1250@gmail.com>
2025-03-13 15:54:39 +0530

    Implement routing with react-router-dom and restructure App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import './App.css';
-import Signup from './components/signup';
-import Login from './components/Login';
-import Header from './components/header';
-import JoinCourse from './components/join_course_student';
-import CreateCourse from './components/create_course_teacher';
-import JoinFirst from './components/joinfirst_student';
-import CreateFirst from './components/createfirstcourse_teacher';
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import "./App.css";
+import Signup from "./components/signup";
+import Login from "./components/Login";
+import Header from "./components/header";
+import JoinCourse from "./components/join_course_student";
+import CreateCourse from "./components/create_course_teacher";
+import JoinFirst from "./components/joinfirst_student";
+import CreateFirst from "./components/createfirstcourse_teacher";
+
+const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <JoinFirst />
-      <CreateFirst />
-      <CreateCourse />
-      <JoinCourse />
-      <Login />
-      <Signup />
-    </div>
+    <Router>
+      <MainLayout />
+    </Router>
   );
-}
+};
+
+const MainLayout = () => {
+  const location = useLocation();
+  const hideHeaderRoutes = ["/", "/signup"];
+  const shouldShowHeader = !hideHeaderRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {shouldShowHeader && <Header />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/join-course" element={<JoinCourse />} />
+        <Route path="/create-course" element={<CreateCourse />} />
+        <Route path="/join-first" element={<JoinFirst />} />
+        <Route path="/create-first" element={<CreateFirst />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
